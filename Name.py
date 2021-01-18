7841ae2747611d517325af7c18058d61bb81c598
unknown <yatin.mehta89@gmail.com>
2021-01-18 23:51:37 +0530

    Version 2 Name.py

--- Name.py
+++ Name.py
@@ -1,2 +1,8 @@
 print("yatindra Mehta is he name ")
+Name=input("Please Enter your name ")
+if Name=='Yatindra':
+    print ('Hello Yatindra')
+else 
+    print('you are  not  correct user')
+    exit
 
